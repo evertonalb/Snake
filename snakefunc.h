b851239d1b0fdc32e835afbdb028f5d91bde90a4
Everton Albuquerque <186333879+evertonalb@users.noreply.github.com>
2025-04-24 23:02:45 -0400

    Declare enum Direction, struct Snake_Segment, and function add_segment
    Direction: will indicate the direction that each segment
    is following. Snake_Segment: structure for each segment
    of the snake. add_segment: adds one segment to the snake's body.

--- snakefunc.h
+++ snakefunc.h
@@ -1,4 +1,17 @@
+// snakefunc.c
 #include <SDL3/SDL.h>
+#include <stdlib.h>
+
+// Direction
+typedef enum {up, right, down, left} Direction;
+
+// Snake body
+typedef struct {
+	int i;
+	int j;
+
+	
+} Snake_Segment;
 
 // Handles responses to key presses.
 void on_key_press(SDL_KeyboardEvent keyboardEvent);
@@ -14,3 +27,6 @@ void color_cell(int i, int j, int sz, SDL_FPoint g[sz+1][sz+1], SDL_Renderer *r,
 
 // Draws the border around the window
 void draw_border(int sz, SDL_FPoint g[sz+1][sz+1], SDL_Renderer *r);
+
+// Adds one segment to the snake array
+void add_segment(Snake_Segment **snake_p, int *sz);
